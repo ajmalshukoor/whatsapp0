4da385e4fa60f0c9aef85a3e5140e292be21bfbe
ajmalshukoor <58746397+ajmalshukoor@users.noreply.github.com>
2021-06-29 21:35:46 -0400

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -4,6 +4,7 @@ function Loading() {
     return (
         <center style={{display: "grid", placeItems: "center", height: "100vh"}}>
             <div>
+                <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" alt="" style={{marginBottom: 10}} width={200}  height={200}/>
                 <Circle color="#3CBC2B" size={60}/>
             </div>
         </center>
